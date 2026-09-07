8480cb14acd7ef1254f429c892fb0abdf77a2f6b
Luis Felipe Lopes <luis.felipe.lopes1275@gmail.com>
2026-09-07 15:17:31 -0400

    feat(Error): Implementa classe de erro RateioError

--- rateio.ts
+++ rateio.ts
@@ -4,9 +4,15 @@ export const MUDAS_POR_BANDEJA = 50;
 
 export type SituacaoCadastral = "regular" | "suspensa" | "irregular";
 
-// Classe que vai lançar alguns erros especificos
+// Classe RateioError que vai lançar alguns erros especificos.
+// 'extends' reconhece a classe RateioError como um erro oficial que herda a classe Error nativa.
+// 'super' ativa o construtor da classe Error nativa com a mensagem que vai ser disparada.
+// A escolha por usar o this.name é para deixar o nome claro e dar mais rastreabilidade ao erro.
 export class RateioError extends Error {
-    
+    constructor(message: string) {
+        super(message);
+        this.name = "RateioError";
+    }
 }
 
 export interface Associacao {
