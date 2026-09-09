057cfe85279c97274cee09a955944e084919f46a
Luis Felipe Lopes <luis.felipe.lopes1275@gmail.com>
2026-09-09 14:48:03 -0400

    feat(Rateio): Finaliza implementação do while com calculos principais do desafio e monta tambem os resultados finais com as regras aplicadas

--- rateio.ts
+++ rateio.ts
@@ -1,3 +1,4 @@
+// rateio.ts
 export const MUDAS_POR_BANDEJA = 50;
 
 export type SituacaoCadastral = "regular" | "suspensa" | "irregular";
@@ -36,10 +37,7 @@ export interface ResultadoRateio {
     sobraNaoDistribuida: number;
 }
 
-export function ratearMudas(
-    totalMudas: number,
-    associacoes: Associacao[]
-): ResultadoRateio {
+export function ratearMudas(totalMudas: number, associacoes: Associacao[]): ResultadoRateio {
 
     // !Number.isInteger já cobre casos onde o valor é NaN, null, undefined, string, números quebrados...
     // tudo que poderia falhar nas validações e causar problemas
@@ -121,7 +119,7 @@ export function ratearMudas(
     const loteBandejas = Math.floor(totalMudas / MUDAS_POR_BANDEJA);
 
     // O edital fala que o objeto deve ser imutável, então faço um novo com os campos novos e já calculados, e o original fica puro
-    const associacoesElegiveis = associacoesValidas.map(associacao => ({
+    const associacoesParticipantes = associacoesValidas.map(associacao => ({
         cnpj: associacao.cnpj,
         nome: associacao.nome,
         familias: associacao.familias,
@@ -134,11 +132,120 @@ export function ratearMudas(
 
     while (true) {
         // Filtra quem ainda pode receber bandejas e não atingiu a cota máxima
-        const associacoesDisponiveis = associacoesElegiveis.filter(a => !a.saturado);
+        const associacoesDisponiveis = associacoesParticipantes.filter(a => !a.saturado);
 
         // Se não for mais possível distribuir, sai do loop
         if(associacoesDisponiveis.length === 0 || bandejasParaDistribuir === 0) {
             break;
         }
+
+        // Aqui usa o reduce para calcula o total de familias de cada associação que vão participar da rodada de distribuição
+        const totalFamilias = associacoesDisponiveis.reduce((soma, a) => soma + a.familias, 0);
+
+        // Calculo da primeira distribuição entre as associações e verifica sobras para nova rodada de distribuição se possível
+        const distribuicaoProvisoria = associacoesDisponiveis.map(associacao => {
+            const cotaCorreta = (bandejasParaDistribuir * associacao.familias) / totalFamilias; // Cálculo da cota a ser distribuída de bandejas para cada associação
+            const cotaInteira = Math.floor(cotaCorreta); // Cálculo da distribuição inteira de bandejas para cada associação
+            const resto = cotaCorreta - cotaInteira; // Cálculo das sobras para uma possível nova rodada de distribuição
+
+            return {
+                associacao: associacao,
+                bandejasTentativas: cotaInteira,
+                resto: resto
+            };
+        })
+
+        // Soma as bandejas que ja foram distribuídas para calcular a sobra para uma nova rodada de distribuição
+        const bandejasDistribuidas = distribuicaoProvisoria.reduce((soma, distribuicao) => soma + distribuicao.bandejasTentativas, 0);
+        let sobraBandejas = bandejasParaDistribuir - bandejasDistribuidas;
+
+        distribuicaoProvisoria.sort((a, b) => {
+            // Quem tiver mais perto de completar uma bandeja inteira, em termos decimais, vai receber primeiro na próxima rodada
+            if(b.resto !== a.resto) {
+                return b.resto - a.resto;
+            }
+
+            // Já aqui, se as associações tiverem o mesmo resto, ganha quem tiver menos famílias, critério de desempate
+            if(a.associacao.familias !== b.associacao.familias) {
+                return a.associacao.familias - b.associacao.familias;
+            }
+
+            // Então se ainda estiver em situação de empate, aqui faz uma comparação por nomes, com o localeCompare
+            // para respeitar a acentuação e a ordem alfabética correta do português brasileiro, ignorando maiúsculas e minúsculas
+            const comparacaoPorNome = a.associacao.nome.localeCompare(b.associacao.nome, "pt-BR");
+            if(comparacaoPorNome !== 0) {
+                return comparacaoPorNome;
+            }
+
+            // E se ainda sim empatar, compara CNPJ, já que são únicos e quem tivr o menor, leva
+            return a.associacao.cnpj.localeCompare(b.associacao.cnpj, "pt-BR");
+        })
+
+        // Agora faz uma nova rodada de distribuição com as sobras, respeitando as regras
+        for(let i = 0; i < sobraBandejas; i++) {
+            const item = distribuicaoProvisoria[i];
+            if(item) {
+                item.bandejasTentativas ++;
+            }
+        }
+
+        // Aqui faz uma verificação de a associação atingiu a cota máxima de bandejas
+        const cotaMaximaAtingida = distribuicaoProvisoria.some(
+            item => item.bandejasTentativas > item.associacao.cotaBandejas
+        );
+
+        // Então se alguma atingiu vai marcar o campo saturado como true e vai retirar essa associação de uma próxima rodada
+        if(cotaMaximaAtingida) {
+            for(const item of distribuicaoProvisoria) {
+                if(item.bandejasTentativas > item.associacao.cotaBandejas) {
+                    item.associacao.bandejas = item.associacao.cotaBandejas;
+                    item.associacao.saturado = true;
+                }
+            }
+
+            // Aqui vai calcular quantas já foram distribuidas novamente para ver as sobras e continuar a distribuição
+            const bandejasJaDistribuidas = associacoesParticipantes.filter(a => a.saturado)
+            .reduce((soma, associacao) => soma + associacao.bandejas, 0);
+
+            bandejasParaDistribuir = loteBandejas - bandejasJaDistribuidas;
+        } else {
+            // Se nenhuma atingiu ainda, só atualiza com a com o número definitivo de bandejas recebidas e sai do loop, acabou as sobras
+            for(const item of distribuicaoProvisoria) {
+                item.associacao.bandejas = item.bandejasTentativas;
+            }
+
+            break;
+        }
+    }
+
+    // Aqui monto a lista final das distribuições, com o número de bandejas e mudas recebidas por cada associação
+    const distribuicoesFinal: Distribuicao[] = associacoesParticipantes.map(associacao => ({
+        cnpj: associacao.cnpj,
+        nome: associacao.nome,
+        bandejas: associacao.bandejas,
+        mudas: associacao.bandejas * MUDAS_POR_BANDEJA
+    }))
+
+    // Junta as 2 listas das distribuições, as que receberam mudas e as que foram excluídas, para montar e ordenar o resultado final
+    const totalDistribuicoesFinal = [ ...distribuicoesFinal, ...distribuidasExcluidas ];
+
+    // O resultado final vai ser ordenado pela associação que tiver mais mudas primeiro, e em caso de empate,
+    // vai ordenar por ordem alfabética com o nome da associação
+    totalDistribuicoesFinal.sort((a, b) => {
+        if(b.mudas !== a.mudas) {
+            return b.mudas - a.mudas;
+        }
+        return a.nome.localeCompare(b.nome, "pt-BR");
+    })
+
+    // Então aqui soma tudo que foi distribuido e as sobras que não foram possíveis de serem distribuídas, para montar o resultado final
+    const totalDistribuido = totalDistribuicoesFinal.reduce((soma, distribuicao) => soma + distribuicao.mudas, 0);
+    const sobraNaoDistribuida = totalMudas - totalDistribuido;
+
+    // Montagem do objeto final com os resultados do rateio
+    return {
+        distribuicoes: totalDistribuicoesFinal,
+        totalDistribuido,
+        sobraNaoDistribuida
     }
 }
